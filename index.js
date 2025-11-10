da26aaa16896e474cc8944a05360472f91cc3f33
Abdul Moiz <moiz05366@gmail.com>
2025-11-10 15:11:07 +0500

    updated

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ import connectDB from "./src/config/db.js";
 import routes from "./src/routes/index.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ENV } from "./src/constants/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(cookieParser());
+app.use("/api", routes);
 
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
 
-app.use(cookieParser());
-
-app.use("/api", routes);
-
-app.listen(3000, () => {
-    console.log("server is running.");
-});
+const PORT = ENV.PORT || 5000;
+if (ENV.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log("server is running.");
+    });
+}
 
 connectDB();
+export default app;
